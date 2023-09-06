3fb675675e73c5c082a0229a611df9a79007ac42
BayronSoto-19 <b.soto54@alumnos.santotomas.cl>
2023-09-05 23:39:30 -0300

    Initial commit

---
+++ app/src/main/java/com/example/spacesport/TecnicasBoxeo.java
@@ -0,0 +1,14 @@
+package com.example.spacesport;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TecnicasBoxeo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tecnicas_boxeo);
+    }
+}
